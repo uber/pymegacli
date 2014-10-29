6b50484cfb5eb1ac208b05408cc1d44fb0a63858
James Brown <jbrown@uber.com>
2014-10-28 23:01:15 -0700

    bump to 0.1.5.2

--- pymegacli/__init__.py
+++ pymegacli/__init__.py
@@ -2,6 +2,6 @@ from .components import MegaCLIBase
 
 __all__ = ['MegaCLIBase']
 
-version_info = (0, 1, 5, 1)
+version_info = (0, 1, 5, 2)
 __version__ = '.'.join(str(c) for c in version_info)
 __author__ = 'James Brown <jbrown@uber.com>'
